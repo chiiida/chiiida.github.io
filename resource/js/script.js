5a6974d91e49645312243a997e40191bdb27faa2
llleyelll <chida.fua@gmail.com>
2020-08-30 19:16:52 +0700

    fix responsive

--- resource/js/script.js
+++ resource/js/script.js
@@ -91,19 +91,4 @@ document.addEventListener('DOMContentLoaded', function (event) {
   StartTextAnimation(0);
 });
 
-
-
-const nodes = [].slice.call(document.querySelectorAll('li'), 0);
-const directions = { 0: 'top', 1: 'right', 2: 'bottom', 3: 'left' };
-const classNames = ['in', 'out'].map((p) => Object.values(directions).map((d) => `${p}-${d}`)).reduce((a, b) => a.concat(b));
-
-const getDirectionKey = (ev, node) => {
-  const { width, height, top, left } = node.getBoundingClientRect();
-  const l = ev.pageX - (left + window.pageXOffset);
-  const t = ev.pageY - (top + window.pageYOffset);
-  const x = (l - (width / 2) * (width > height ? (height / width) : 1));
-  const y = (t - (height / 2) * (height > width ? (width / height) : 1));
-  return Math.round(Math.atan2(y, x) / 1.57079633 + 5) % 4;
-}
-
 AOS.init();
